d38be7079206285c8016cd7379eb46c2ab199539
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-28 20:43:21 +0100

    feat(app): ✨ adding Task date validation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# todolist
- A Todo List app made with JAVA 22
+# Todo List App
+
+ A Todo List app made with [JAVA 22](https://jdk.java.net/22/), [Apache Maven](https://maven.apache.org) and [Spring Boot](https://start.spring.io)
+
+## Functional Requirements
+
+- [x] Should be possible to Register a new User
+- [x] Should be possible to Login with the User
+- [x] Should be possible to Add a Task
+
+## Business Requirements
+
+- [x] Should not be possible to create a Task with an title with more than 50 characters
+- [x] Should not be possible to create a Task with an date in the past from today
+- [x] Should not be possible to create a Task with Start Date in the future than End Date
+- [x] Should not be possible to create a Task with Start Date equal to End Date
+- [x] A Task can only be possible to be created by an authenticated User
+
+## Non-Functional Requirements
+
+- [x] User must be authenticated with Basic Auth
--- src/main/java/pro/euclides/todolist/task/TaskController.java
+++ src/main/java/pro/euclides/todolist/task/TaskController.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +21,24 @@ public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
 
+    @SuppressWarnings("rawtypes")
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         UUID userId = (UUID) request.getAttribute("userId");
         taskModel.setUserId(userId);
-        return this.taskRepository.save(taskModel);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST).body("Start and End date must be in the future");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST).body("Start date must be before End date");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 }
