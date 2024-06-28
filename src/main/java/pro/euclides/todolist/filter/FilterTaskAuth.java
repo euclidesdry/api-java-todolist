abad52b178e24fc1b4d19cae9e71940a55ad134e
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-28 19:39:57 +0100

    feat(filters): ✨ adding User password decode

--- src/main/java/pro/euclides/todolist/filter/FilterTaskAuth.java
+++ src/main/java/pro/euclides/todolist/filter/FilterTaskAuth.java
@@ -3,16 +3,23 @@ package pro.euclides.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import pro.euclides.todolist.user.IUserRepository;
+
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @SuppressWarnings("null")
     @Override
@@ -27,6 +34,21 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String[] credentials = authDecodedString.split(":");
         String username = credentials[0];
         String password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No user found");
+            return;
+        } else {
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (passwordVerify.verified) {
+                request.setAttribute("user", user);
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Wrong password");
+                return;
+            }
+        }
 
         filterChain.doFilter(request, response);
     }
