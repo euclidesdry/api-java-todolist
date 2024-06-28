e6569f4805ee286152ace7fc1782c230d2ae55f5
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-28 16:25:07 +0100

    feat(repositories): ✨ adding User repository

--- src/main/java/pro/euclides/todolist/user/UserController.java
+++ src/main/java/pro/euclides/todolist/user/UserController.java
@@ -3,14 +3,19 @@ package pro.euclides.todolist.user;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
